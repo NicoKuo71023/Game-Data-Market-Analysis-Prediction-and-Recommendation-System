957b0d5da0ae1ccd9ec8864246a50c9098e931a1
nico71023 <nico2033210332@gmail.com>
2024-10-24 22:10:33 +0800

    1.4.2 ML/DL instructed 102424

--- Module/ES_connector.py
+++ Module/ES_connector.py
@@ -1,18 +1,26 @@
 import requests
 import json
+from requests.auth import HTTPBasicAuth
 
 class ElasticsearchQuery:
-    def __init__(self, es_host, index_name):
+    def __init__(self, es_host, index_name, api_key=None):
         self.es_host = es_host
         self.index_name = index_name
+        self.api_key = api_key  # API key 是可選的
+
+    def get_auth(self):
+        """根據是否有 API key 返回適當的身份驗證方法"""
+        if self.api_key:
+            return HTTPBasicAuth(self.api_key[0], self.api_key[1])
+        return None  # 如果沒有 API key，返回 None
 
     def get_count(self):
         # 構建 _count 請求的 URL
         endpoint = f'/{self.index_name}/_count'
         url = f'{self.es_host}{endpoint}'
         
-        # 發送 GET 請求
-        response = requests.get(url)
+        # 發送 GET 請求，根據是否有 API key 決定是否帶上身份驗證
+        response = requests.get(url, auth=self.get_auth())
         
         # 檢查回應狀態碼並返回 count 值
         if response.status_code == 200:
@@ -35,8 +43,9 @@ class ElasticsearchQuery:
         # 更新 query，設定 size 為 count
         query['size'] = count
         
-        # 發送 POST 請求進行查詢
-        response = requests.post(url, headers={"Content-Type": "application/json"}, data=json.dumps(query))
+        # 發送 POST 請求進行查詢，根據是否有 API key 決定是否帶上身份驗證
+        response = requests.post(url, headers={"Content-Type": "application/json"},
+                                 data=json.dumps(query), auth=self.get_auth())
         
         # 檢查回應狀態碼並返回查詢結果
         if response.status_code == 200:
@@ -48,12 +57,15 @@ class ElasticsearchQuery:
 
 
 if __name__ == "__main__":
-    # 定義 Elasticsearch 伺服器和索引
-    es_host = 'http://192.168.31.130:32327'
+    # 定義 Elasticsearch 雲端伺服器和索引
+    es_host = 'https://e32aaa94f81549368f75827f1e5da659.us-central1.gcp.cloud.es.io/'
     index_name = 'game_data_aaa'
     
+    # 你可以選擇是否提供 API key
+    api_key = ("g1i_hJIBR8AYR_FZHqsZ", "NgU5E6CkSJmd1t8uuljJJA")  # 或者設置為 None
+    
     # 初始化 ElasticsearchQuery 類
-    es_query = ElasticsearchQuery(es_host, index_name)
+    es_query = ElasticsearchQuery(es_host, index_name, api_key)
     
     # 定義查詢的 query
     query = {
@@ -67,4 +79,4 @@ if __name__ == "__main__":
     
     # 顯示查詢結果
     if search_result:
-        print(search_result[751])
+        print(search_result[0])  # 顯示第一個結果
